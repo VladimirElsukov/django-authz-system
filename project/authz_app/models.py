f654f9ed6a85730c9e7680fee889a27869f5aa59
Vladimir Elsukov <groupdirect@yandex.ru>
2025-11-24 14:37:36 +0300

    Создана расширенная модель пользователя CustomUser

--- project/authz_app/models.py
+++ project/authz_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Расширенная модель пользователя с уникальными именами для обратных связей
+    """
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='группы',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='customuser_groups',  # Уникальное имя обратной связи
+        related_query_name='customuser_group',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='разрешения пользователя',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='customuser_perms',  # Уникальное имя обратной связи
+        related_query_name='customuser_perm',
+    )
+
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    middle_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)

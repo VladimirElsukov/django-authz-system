965be76514bcc6017502a6116c43ab5380da11d8
Vladimir Elsukov <groupdirect@yandex.ru>
2025-11-25 10:44:26 +0300

    В admin.py добавили возможность добавлять роли пользователям

--- project/authz_app/admin.py
+++ project/authz_app/admin.py
@@ -1,3 +1,21 @@
+# admin.py
 from django.contrib import admin
+from .models import CustomUser, Role, Permission
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'is_active']
+    search_fields = ['email', 'first_name', 'last_name']
+    filter_horizontal = ('roles',)  # Горизонтальный выбор ролей
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ['role', 'resource', 'action']
+    search_fields = ['role__name', 'resource', 'action']
+
+# Регистрируем модели с нашими настройками
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Role, RoleAdmin)
+admin.site.register(Permission, PermissionAdmin)
